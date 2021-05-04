e86733ca9a79be71d6be9283e97aade4bce736e8
mikee-guess44 <pes2017b1@gmail.com>
2021-05-04 18:18:09 -0300

    navbar update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,43 @@
 import Reef from 'reefjs';
 
 let storeNavbar = new Reef.Store({
-  data: {}
+  data: {
+    styles: {
+      buttonWallet: {
+        display: 'true',
+        loading: 'false'
+      }
+    },
+    content: {
+      user: {
+        display: '',
+        address: '',
+        avatar: ''
+      }
+    },
+    files: { value: [] }
+  },
+  setters: {
+    setStyle: function (props, todos) {
+
+      for (var i = 0; i < todos.length; i++) {
+
+       let _elmt = props.styles[todos[i].element];
+       todos[i].value != '' ? _elmt[todos[i].style] = todos[i].value : _elmt[todos[i].style] = ''
+      }
+
+    },
+    setContent: function (props, todos) {
+
+      for (var i = 0; i < todos.length; i++) {
+
+       let td = props.content[todos[i].element];
+       todos[i].value != '' ? td[todos[i].field] = todos[i].value : td[todos[i].field] = ''
+      }
+
+    }
+  },
+  getters: {}
 })
 export const NavBar = new Reef('#nav-bar', {
   store: storeNavbar,
